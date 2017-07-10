39e458b336a83d45c2d66d667b77f47a5edde296
mikebcbc <mikebconstantino@gmail.com>
2017-07-10 17:55:59 -0400

    GET, POST, and PUT implemeted

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const morgan = require('morgan');
 const app = express();
 
 const blogPostRouter = require('./blogPostRouter');
+
+app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(8080, () => {
+	console.log('We on port 8080.');
+})
